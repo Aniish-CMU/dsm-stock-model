5ee48e116fdba358b23eb39e3503fbba11097977
Aniish Sridhar <aniishs@andrew.cmu.edu>
2023-06-17 10:48:41 +0300

    Reading data through a .csv

--- application/__init__.py
+++ application/__init__.py
@@ -23,21 +23,21 @@ def data_preprocess(start_time, raw_data):
 
 def get_historical_data(symbol, start_date = None):
     df = pd.read_csv(symbol+".csv")
-    api_key = open(r'api_key.txt')
-    api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={api_key}&outputsize=full'
-    companyname_dict = {"Apple": "AAPL", "Amazon": "AMZN", "Meta": "META"}
+    #api_key = open(r'api_key.txt')
+   # api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={api_key}&outputsize=full'
+    #companyname_dict = {"Apple": "AAPL", "Amazon": "AMZN", "Meta": "META"}
     
-    raw_df = requests.get(api_url).json()
-    df = pd.DataFrame(raw_df[f'Time Series (Daily)']).T
-    df = df.rename(columns = {'1. open': 'open', '2. high': 'high', '3. low': 'low', '4. close': 'close', '5. adjusted close': 'adj close', '6. volume': 'volume'})
-    for i in df.columns:
-        df[i] = df[i].astype(float)
-    df.index = pd.to_datetime(df.index)
-    df = df.iloc[::-1].drop(['7. dividend amount', '8. split coefficient'], axis = 1)
+    #raw_df = requests.get(api_url).json()
+   # df = pd.DataFrame(raw_df[f'Time Series (Daily)']).T
+    #df = df.rename(columns = {'1. open': 'open', '2. high': 'high', '3. low': 'low', '4. close': 'close', '5. adjusted close': 'adj close', '6. volume': 'volume'})
+   # for i in df.columns:
+   #     df[i] = df[i].astype(float)
+   # df.index = pd.to_datetime(df.index)
+   # df = df.iloc[::-1].drop(['7. dividend amount', '8. split coefficient'], axis = 1)
 
-    if start_date:
-         df = df[df.index >= start_date]
-    df.to_csv(symbol+".csv")
+   # if start_date:
+       #  df = df[df.index >= start_date]
+   # df.to_csv(symbol+".csv")
     df_prophet = data_preprocess("2019-01-02", df)
     model = Prophet()
     model.fit(df_prophet)
